9ea8aef1ce6cec5060cb0534a05500bb25df115c
Hanif2601 <hnf.ssg2601@gmail.com>
2025-03-12 08:15:13 +0700

    pratikum 3

---
+++ semester2/pertemuan_3/admin/index.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'top.php';
+
+?>
+
+<div id="page-content-wrapper">      
+    <?php 
+        include_once 'menu.php';
+    ?>
+                <!-- Page content-->
+                <div class="container-fluid">
+                  
+                    <!-- <h1 class="mt-4">Selamat Datang</h1> -->
+                    <h1>Selamat datang di web saya hanif fatih azzam</h1>
+                </div>
+            </div>
+        <?php
+        include_once 'bottom.php';
+        ?>
